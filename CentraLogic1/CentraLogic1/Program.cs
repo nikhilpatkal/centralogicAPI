543a04675b853cad103a0e930895c4b9d78862c6
Nikhil Patkal <nikhilpatkal9552@gmail.com>
2025-06-15 18:56:32 +0530

    Initial commit - .NET Core

---
+++ CentraLogic1/CentraLogic1/Program.cs
@@ -0,0 +1,10 @@
+Console.WriteLine("Enter the Name");
+
+string name =System.Console.ReadLine();
+Console.WriteLine("The name enter  by the user"+name);
+
+Mymethod();
+void Mymethod()
+{
+  Console.WriteLine("you are in the method");
+}
